84e5c599b2f7f3b11f5828805887710112184644
GuillermoBaldan <guillermobaldan@gmail.com>
2021-10-22 05:50:30 +0200

    flicker bug fixed. Fixes #13

--- ZeroPlayers_f_level1.js
+++ ZeroPlayers_f_level1.js
@@ -21,29 +21,27 @@ function init(stageParameters,simulationParameters){
         //2.staticStage
         stageParameters.staticStage = generateStaticStage(stageParameters,simulationParameters);
         //3.Add dynamic Elements
-        for(a=0;a<2;a++){
-            stageParameters.dynamicElementsArray.push(new simpleCell);
-        }
-
         for(a=0;a<100;a++){
             stageParameters.dynamicElementsArray.push(new grossCell);
         }
 
-       matrixAux = matrixGeneratorInit(stageParameters,simulationParameters);
+        for(a=0;a<2;a++){
+            stageParameters.dynamicElementsArray.push(new simpleCell);
+        }
+        matrixAux = matrixGeneratorInit(stageParameters,simulationParameters);
         //4. Draw canvas
         drawingMatrix(matrixAux, simulationParameters);
         return [staticStageAux, matrixAux ,canvas[0], canvas[1]];//lienzo = canvas[0];ctx = canvas[1]
     } else {
-        console.log("Los datos no son coherentes")
+        console.log("The data is not consistent")
     }
-
 }
+
 //simulation(init_output[0],dynamicElementsArray,simulationSteps,timePerStep, wideDimension, squareSide,init_output[3])
 function simulation(stageParameters,simulationParameters){
     //1. Hacemos la simulación paso a paso.
    //simulationParameters.globalSimulationIndex =  oneSimulationStep(stageParameters,simulationParameters)
    oneSimulationStep(stageParameters,simulationParameters)
-   //return simulationIndexAux;
-   }
+}
 
 export { init, simulation }
