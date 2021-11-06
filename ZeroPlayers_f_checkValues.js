3e5bcad402f1450474950638938603ac04bac285
GuillermoBaldan <guillermobaldan@gmail.com>
2021-11-06 22:18:14 +0100

    debuging exceeds of objects bug

--- ZeroPlayers_f_checkValues.js
+++ ZeroPlayers_f_checkValues.js
@@ -12,9 +12,11 @@ function checkNumbersTypeCell(className, stageParameters){
     let a;
     let counter = 0;
     for(a=0;a<stageParameters.dynamicElementsArray.length;a++){
-        if(stageParameters.dynamicElementsArray[a].constructor == className){
+        if(stageParameters.dynamicElementsArray[a].constructor.name == className){
+            
             counter += 1
         }
+        //console.log(`constructor.name ${stageParameters.dynamicElementsArray[a].constructor.name}`)
     }
     return counter;
 }
