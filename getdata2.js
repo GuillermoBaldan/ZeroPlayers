ba70d8c869a65869fc289ddce9074220b95dd6c2
GuillermoBaldan <guillermobaldan@gmail.com>
2021-12-08 23:56:42 +0100

    async function getdata

--- getdata2.js
+++ getdata2.js
@@ -43,29 +43,37 @@ const getData = () => {
 
 getBtn.addEventListener("click", getData);
  */
-var data = JSON.stringify({
+let data = JSON.stringify({
   login: "Baldan",
   password: "Hero",
 });
 
-var xhr = new XMLHttpRequest();
-//xhr.withCredentials = true;
+let getData = new AsyncFunction(data,  
+  
+  let xhr = new XMLHttpRequest();
+  //xhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    console.log(xhr.responseText);
-  }
-});
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      //console.log(xhr.responseText);
+      return xhr.responseText;
+    }
+  
 
-xhr.open("GET", "http://localhost:6001/usuarios/619420df977695930002adb6");
-xhr.setRequestHeader(
-  "token",
-  "bearer - eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTk0MjBkZjk3NzY5NTkzMDAwMmFkYjYiLCJsb2dpbiI6IkJhbGRhbiIsInJvbCI6IkNMSUVOVEUiLCJtb3ZpZGEiOiJBQkNERUYiLCJpYXQiOjE2Mzg2NTg3NDd9.9ebMdHDgH45DjA1G5D4KZVqaOTU0GFcu47alVjms2KgkzSdXFTN0DxjCSBzOYl9G1KxkmWNEJHDUhFsdid-SQQ"
-);
-xhr.setRequestHeader(
-  "Authorization",
-  "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTk0MjBkZjk3NzY5NTkzMDAwMmFkYjYiLCJsb2dpbiI6IkJhbGRhbiIsInJvbCI6IkNMSUVOVEUiLCJtb3ZpZGEiOiJBQkNERUYiLCJpYXQiOjE2Mzg4MTQ5NTd9.B4Cv0TzM0v416jIzTxSm6Ix3kNNE-AJ-W9BWG-N6o4c_WuzsNQuyFDDLBaB8k049lJuSxiZbyNsh2Y41ViaRVw"
-);
-xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.open("GET", "http://localhost:6001/usuarios/619420df977695930002adb6");
+  xhr.setRequestHeader(
+    "token",
+    "bearer - eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTk0MjBkZjk3NzY5NTkzMDAwMmFkYjYiLCJsb2dpbiI6IkJhbGRhbiIsInJvbCI6IkNMSUVOVEUiLCJtb3ZpZGEiOiJBQkNERUYiLCJpYXQiOjE2Mzg2NTg3NDd9.9ebMdHDgH45DjA1G5D4KZVqaOTU0GFcu47alVjms2KgkzSdXFTN0DxjCSBzOYl9G1KxkmWNEJHDUhFsdid-SQQ"
+  );
+  xhr.setRequestHeader(
+    "Authorization",
+    "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTk0MjBkZjk3NzY5NTkzMDAwMmFkYjYiLCJsb2dpbiI6IkJhbGRhbiIsInJvbCI6IkNMSUVOVEUiLCJtb3ZpZGEiOiJBQkNERUYiLCJpYXQiOjE2Mzg4MTQ5NTd9.B4Cv0TzM0v416jIzTxSm6Ix3kNNE-AJ-W9BWG-N6o4c_WuzsNQuyFDDLBaB8k049lJuSxiZbyNsh2Y41ViaRVw"
+  );
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-xhr.send(data);
+  xhr.send(data);
+})
+
+getData(data).then(response => {
+  console.log(response);
+})
