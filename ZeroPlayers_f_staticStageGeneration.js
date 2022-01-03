14bbc01831a689cf078d44ca46e9fc6220859161
GuillermoBaldan <guillermobaldan@gmail.com>
2022-01-03 04:08:05 +0100

    islandGeneration algorithm development part 2

--- ZeroPlayers_f_staticStageGeneration.js
+++ ZeroPlayers_f_staticStageGeneration.js
@@ -44,6 +44,10 @@ function random(stageParameters, simulationParameters){
     let b;
     let row = [];
     let staticStageAux = [];
+    let counter = 0;
+    let vuelta = 1;
+    let numberUnits = 4;
+    let safe = 0;
     //Primero pintamos todo de azul
     for (
       b = 0;
@@ -67,10 +71,36 @@ function random(stageParameters, simulationParameters){
         );
       }
       staticStageAux.push(row);
-      stageParameters.staticStage = staticStageAux;
+      
     }
-   
-    stageParameters.staticStage[origin[0]][origin[1]] = "brown";
+   //2 Generamos la isla
+    staticStageAux[origin[1]][origin[0]] = "red";
+   do {
+           if(staticStageAux[origin[1] + vuelta][origin[0]] == "blue"){
+        staticStageAux[origin[1] + vuelta][origin[0]] = "brown";
+        counter++;
+        console.log("Se mete en el primer if")
+      } else if (staticStageAux[origin[1]][origin[0] - vuelta] == "blue"){
+        staticStageAux[origin[1]][origin[0] - vuelta] = "brown";
+        counter++;
+        console.log("Se mete en el segundo if")
+        console.log(staticStageAux[origin[1] - vuelta][origin[0]])
+      } else if (staticStageAux[origin[1] - vuelta][origin[0]] == "blue"){
+        staticStageAux[origin[1] - vuelta][origin[0]] = "brown";
+        counter++;
+        console.log("Se mete en el tercer if")
+
+      } else if(staticStageAux[origin[1]][origin[0] + vuelta] == "blue"){
+        staticStageAux[origin[1]][origin[0] + vuelta] = "brown";
+        counter++;
+        console.log("Se mete en el cuarto if")
+      } 
+      safe++;
+    } while(counter<numberUnits && safe<100)
+    
+
+    
+    stageParameters.staticStage = staticStageAux;
     return stageParameters.staticStage;
   }
   
