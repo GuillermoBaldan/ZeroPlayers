b540b3ebf820e9de998b5d8c1184e36393ff2cab
GuillermoBaldan <guillermobaldan@gmail.com>
2022-02-02 23:17:28 +0100

    predator class added

--- ZeroPlayers_classes_livingBeings.js
+++ ZeroPlayers_classes_livingBeings.js
@@ -183,4 +183,48 @@ class vegetable {
 }
 }
 
-export { grossPredator, grossCell, yellowPredator, vegetable };
+class predator {
+  constructor() {
+    this.type = "predator"
+    this.color = "purple";
+    this.x = Math.floor(
+      Math.random() *
+        (simulationParameters.wideDimension / simulationParameters.squareSide)
+    ); //Math.random() * (max - min) + min;
+    this.y = Math.floor(
+      Math.random() *
+        (simulationParameters.heightDimension / simulationParameters.squareSide)
+    );
+    this.life = 100;
+    this.walkmode = "autonomous";
+    this.trajectory_x = [1, 1, 1, 1, 1, 1, 1];
+    this.trajectory_y = [0, 0, 0, 0, 0, 0, 0];
+    this.walk = totalFreedom;
+    this.life = 300;
+    this.maxEnergy = 250;
+    this.energyBorn = this.maxEnergy / 2;
+    this.energy = this.energyBorn;
+    this.lifeConsumption = 1;
+    this.energyConsumption = 1;
+    this.behaviourRules = {
+      forbiddenPositions: ["water", "simpleCell"],
+      forbiddenColors: ["blue", "yellow", "purple"],
+    };
+    this.preyClasses = [grossPredator];
+    this.reproductionRadio = 1;
+    this.reproductionPeriod = 4;
+    this.cyclesToReproduction = Math.round(Math.random()*this.reproductionPeriod)
+    this.vitalFunctions = {
+      death: true,
+      reproduction: true,
+      prey: true,
+      }
+    this.cognitiveFunctions = {
+      see: true,
+      pathfinder: true,
+    }
+    
+  }
+}
+
+export { grossPredator, grossCell, yellowPredator, vegetable, predator };
