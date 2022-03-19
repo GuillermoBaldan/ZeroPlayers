9dc14488ec0e1c3e54d24806aadb195e444f5a58
GuillermoBaldan <guillermobaldan@gmail.com>
2022-03-19 17:18:50 +0100

    Fixes #51

--- backend/simple-main.js
+++ backend/simple-main.js
@@ -1,8 +1,8 @@
 //Do a script to serve a web using node
 const express = require('express');
 const app = express();
-
-app.use(express.static(__dirname + '../public/'));
+//Serves the contend of the folder frontend
+app.use(express.static(__dirname + '../../frontend/'));
 
 app.listen('3000', function() {
   console.log('Server listening on port 3000');
