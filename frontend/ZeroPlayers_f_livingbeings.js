67b0ae02528ee6f1b7b8f566a2f4937dbc63dbe5
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-03 23:35:45 +0200

    snailSection algorithm implementation

--- frontend/ZeroPlayers_f_livingbeings.js
+++ frontend/ZeroPlayers_f_livingbeings.js
@@ -473,6 +473,45 @@ function hunterPathFinderv2(hunter, stageParameters, simulationParameters) {
     }
   });
 
+  
+  function snailSelection(x, y) {
+    let temp = [];
+    let iterationCounter = 1;
+    let i = 0;
+    flag = false;
+    do {
+      //hace una iteración hasta que encuentra una presa;
+      //Cuadrante 1 - recorremos y de arriba abajo
+      for (i = iterationCounter; i > -iterationCounter; i--) {
+        if (stageParameters.matrix[y + i][x + iterationCounter] != "green") {
+          temp.push([x + iterationCounter, y + i]);
+        }
+      }
+      //Cuadrante 2 - recorremos x de derecha a izquierda
+      for (i = iterationCounter; i > -iterationCounter; i--) {
+        if (stageParameters.matrix[y - iterationCounter][x + i] != "green") {
+          temp.push([x + i, y - iterationCounter]);
+        }
+      }
+      //Cuadrante 3 - recorremos y de abajo arriba
+      for (i = -iterationCounter; i < iterationCounter; i++) {
+        if (stageParameters.matrix[y + i][x - iterationCounter] != "green") {
+          temp.push([x - iterationCounter, y + i]);
+        }
+      }
+      //Cuadrante 4 - recorremos x de izquierda a derecha
+      for (i = -iterationCounter; i < iterationCounter; i++) {
+        if (stageParameters.matrix[y + iterationCounter][x + i] != "green") {
+          temp.push([x + i, y + iterationCounter]);
+        }
+      }
+
+      iterationCounter += 1;
+    } while (flag == "false");
+
+    return temp;
+  }
+
   //2. Calculate the path to the preys
 
   if (preyArray.length > 0) {
@@ -533,6 +572,10 @@ function hunterPathFinderv3(hunter, stageParameters, simulationParameters) {
   }
 
   return result;
+}
+
+function hunterPathFinderv4(hunter, stageParameters, simulationParameters) {
+  //This algorith is based on Snail Selection Algorithm
 }
 
 function hunterGroupPathFinder(hunter, stageParameters, simulationParameters) {
