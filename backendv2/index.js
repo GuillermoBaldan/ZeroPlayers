6b51775bfaa9664188e3b2200b50d44d1459f574
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-07 23:20:39 +0200

    create index.js at backend v2 - workpart 09/05/2022

---
+++ backendv2/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+//Initializations
+const app = express();
+require("./database");
+
+//Setting
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+//Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "mysecretapp",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+//Glbal Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+//Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/notes"));
+app.use(require("./routes/users"));
+
+//Static Files
+app.use(express.static(path.join(__dirname, "public")));
+
+//Server is listenning
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
+
+/* Código final
+https://github.com/FaztTech/nodejs-notes-app */
