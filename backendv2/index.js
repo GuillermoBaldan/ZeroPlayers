5dd2a6c044bc4bc992b35081b51cf3cc81223c9d
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-10 23:45:07 +0200

    game.js file of routes added - workpart 19/05/2022

--- backendv2/index.js
+++ backendv2/index.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 //Routes
 app.use(require("./routes/index"));
-app.use(require("./routes/notes"));
+app.use(require("./routes/game"));
 app.use(require("./routes/users"));
 
 //Static Files
