243d2d13555714a3921c0493b7aecd01035693b6
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-23 16:15:37 +0200

    Game.js of models updated!

--- backendv2/models/Game.js
+++ backendv2/models/Game.js
@@ -2,126 +2,146 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const GameSchema = new Schema({
-  stageParameters : {
-      universeRules : {
-            movementType : {
-               type: String,
-                enum: ["zigzag", "diagonal"],
-                default: "zigzag",
-                required: true
-            },
-            frontier : {
-                type: String,
-                enum: ["close", "adjacent ends"],
-                default: "close",
-                required: true
-            }
-  },
-  livingBeingsRules : {
-        reproduction : {
-            type: {
-            type: String,
-            enum: ["sexual", "asexual"],
-            default: "sexual",
-            required: true
-            },
-            probability : {
-                type: Number,
-                default: 0.5,
-                required: true
-            },
-            distantTowater : {
-                type: Number,
-                default: 1,
-                required: true
-
+  stageParameters: {
+    universeRules: {
+      movementType: {
+        type: String,
+        enum: ["zigzag", "diagonal"],
+        default: "zigzag",
+        required: true,
+      },
+      frontier: {
+        type: String,
+        enum: ["close", "adjacent ends"],
+        default: "close",
+        required: true,
+      },
+    },
+    livingBeingsRules: {
+      reproduction: {
+        type: {
+          type: String,
+          enum: ["sexual", "asexual"],
+          default: "sexual",
+          required: true,
         },
-        proximityTosameCells : {
-            type: Number,
-            default: 3,
-            required: true
-        }
+        probability: {
+          type: Number,
+          default: 0.5,
+          required: true,
+        },
+        distantTowater: {
+          type: Number,
+          default: 1,
+          required: true,
+        },
+        proximityTosameCells: {
+          type: Number,
+          default: 3,
+          required: true,
+        },
+      },
     },
     legendTerrain: {
-        ground: {
-            type: String,
-            default: "brown",
-            required: true
-        },
-        water: {
-            type: String,
-            default: "blue",
-            required: true
-        }
-    },
-    legend: {
-        water: {
-            type: String,
-            default: "blue",
-            required: true
-        },
-        simpleCell: {
-            type: String,
-            default: "yellow",
-            required: true
-        }
-    },
-    legendForbiddenColors: [{
+      ground: {
+        type: String,
+        default: "brown",
+        required: true,
+      },
+      water: {
         type: String,
         default: "blue",
-        required: true
+        required: true,
+      },
     },
-    {
+    legend: {
+      water: {
+        type: String,
+        default: "blue",
+        required: true,
+      },
+      simpleCell: {
         type: String,
         default: "yellow",
-        required: true
-    },{
+        required: true,
+      },
+    },
+    legendForbiddenColors: [
+      {
+        type: String,
+        default: "blue",
+        required: true,
+      },
+      {
+        type: String,
+        default: "yellow",
+        required: true,
+      },
+      {
         type: String,
         default: "green",
-        required: true
-    },{
+        required: true,
+      },
+      {
         type: String,
         default: "purple",
-        required: true
-    }],
-    livingBeingsCollection: [{
+        required: true,
+      },
+    ],
+    livingBeingsCollection: [
+      {
         name: {
-            type: String,
-            default: "gross predator",
-            required: true
+          type: String,
+          default: "gross predator",
+          required: true,
         },
         type: {
-            type: String,
-            enum: ["predator", "vegetable"],
-            default: "predator",
-            required: true
+          type: String,
+          enum: ["predator", "vegetable"],
+          default: "predator",
+          required: true,
         },
         color: {
-            type: String,
-            default: "yellow",
-            required: true
+          type: String,
+          default: "yellow",
+          required: true,
         },
-        preys: [{
+        preys: [
+          {
             type: String,
             default: "gross",
-            required: true
-        }],
+            required: true,
+          },
+        ],
         movement: {
-            type: String,
-            enum: ["path finder", "None", "Random"],
-            default: "path finder",
-            required: true
+          type: String,
+          enum: ["path finder", "None", "Random"],
+          default: "path finder",
+          required: true,
         },
         number: {
-            type: Number,
-            default: 1,
-            required: true
-        }
+          type: Number,
+          default: 1,
+          required: true,
+        },
+      },
+    ],
+    dynamicElementsArray: [],
+    staticStage: [],
+    matrix: [],
+    freePlacesArray: [],
+    universeEnergy: {
+      type: Number,
+      default: 500000,
+      required: true,
+    },
+    generationStageAlgorithm: {
+      type: String,
+      default: "allTerrain",
+      required: true,
+    },
+    speciesCounter: [],
+  },
+});
 
-
-    }],
-
-
-
-}});
 module.exports = mongoose.model("Game", GameSchema);
