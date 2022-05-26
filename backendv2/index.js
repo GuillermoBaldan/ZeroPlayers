af928fb0387f99c578c2c91b86aa1e9f19d8751f
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-27 01:36:53 +0200

    simulation term change in index.js and file renamed - workpart 04/06/2022

--- backendv2/index.js
+++ backendv2/index.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 //Routes
 app.use(require("./routes/index"));
-app.use(require("./routes/game"));
+app.use(require("./routes/simulations"));
 app.use(require("./routes/users"));
 
 //Static Files
