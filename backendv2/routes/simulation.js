677b70df84218fd7d1e55136c39ff26864491a14
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-27 01:33:06 +0200

    simulation.js of routes modified - workpart 03/06/2022

--- backendv2/routes/simulation.js
+++ backendv2/routes/simulation.js
@@ -3,57 +3,65 @@ const router = require("express").Router();
 const Game = require("../models/Game");
 const { isAuthenticated } = require("../helpers/auth");
 
-router.get("/notes/add", isAuthenticated, (req, res) => {
-  res.render("notes/new-notes");
+router.get("/simulations/add", isAuthenticated, (req, res) => {
+  res.render("simulations/new-simulation");
 });
 
-router.post("/notes/new-note", isAuthenticated, async (req, res) => {
-  const { title, description } = req.body;
-  const errors = [];
-  if (!title) {
-    errors.push({ text: "Please Write a title" });
+router.post(
+  "/simulations/new-simulation",
+  isAuthenticated,
+  async (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if (!title) {
+      errors.push({ text: "Please Write a title" });
+    }
+    if (!description) {
+      errors.push({ text: "Please Write a description" });
+    }
+    if (errors.length > 0) {
+      res.render("simulations/new-simulation", {
+        errors,
+        title,
+        description,
+      });
+    } else {
+      const newNote = new Note({ title, description });
+      newNote.user = req.user._id;
+      await newNote.save();
+      req.flash("success_msg", "Note Added Successfully");
+      res.redirect("/simulations");
+    }
   }
-  if (!description) {
-    errors.push({ text: "Please Write a description" });
-  }
-  if (errors.length > 0) {
-    res.render("notes/new-notes", {
-      errors,
-      title,
-      description,
-    });
-  } else {
-    const newNote = new Note({ title, description });
-    newNote.user = req.user._id;
-    await newNote.save();
-    req.flash("success_msg", "Note Added Successfully");
-    res.redirect("/notes");
-  }
-});
+);
 
-router.get("/notes", isAuthenticated, async (req, res) => {
+router.get("/simulations", isAuthenticated, async (req, res) => {
   const notes = await Note.find({ user: req.user._id })
     .sort({ date: "desc" })
     .lean();
-  res.render("notes/all-notes", { notes });
+  res.render("simulations/all-simulations", { notes });
 });
 
-router.get("/notes/edit/:id", isAuthenticated, async (req, res) => {
+router.get("/simulations/edit/:id", isAuthenticated, async (req, res) => {
   const note = await Note.findById(req.params.id).lean();
-  res.render("notes/edit-note", { note });
+  res.render("simulations/edit-simulation", { note });
 });
 
-router.put("/notes/edit-note/:id", isAuthenticated, async (req, res) => {
-  const { title, description } = req.body;
-  await Note.findByIdAndUpdate(req.params.id, { title, description });
-  req.flash("success_msg", "Note Updated Successfully");
-  res.redirect("/notes");
-});
+router.put(
+  "/simulations/edit-simulation/:id",
+  isAuthenticated,
+  async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash("success_msg", "Note Updated Successfully");
+    res.redirect("/simulations");
+  }
+);
 
-router.delete("/notes/delete/:id", isAuthenticated, async (req, res) => {
+router.delete("/simulations/delete/:id", isAuthenticated, async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
-  req.flash("success_msg", "Note Deleted Successfully");
-  res.redirect("/notes");
+  req.flash("success_msg", "Simulation Deleted Successfully");
+  res.redirect("/simulations");
 });
 
 module.exports = router;
