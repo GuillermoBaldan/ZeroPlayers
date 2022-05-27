f057cb70f7d1a2b1101aee1a375de8bea2ccf428
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-27 10:05:45 +0200

    passport.js file of config folder created - workpart 15/06/2022

---
+++ backendv2/config/passport.js
@@ -0,0 +1,33 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const User = require("../models/User");
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return done(null, false, { message: "Not user found" });
+      } else {
+        const match = await user.matchPassword(password);
+        if (match) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  }).lean();
+});
